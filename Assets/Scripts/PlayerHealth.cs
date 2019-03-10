61b5f291c6ebbc8f998f4fe620a3e11212f64be3
SimonLiu <liusihui@outlook.com>
2019-03-10 18:24:32 +0800

    3rd Version_ShootingNotFinished

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,21 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerHealth : MonoBehaviour {
+public class PlayerHealth : MonoBehaviour
+{
+      
+    public int health;
+    public bool isAlive = true;
 
-	public int health = 10;			//玩家的生命值
-	public bool isAlive = true;		//玩家是否存活
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            isAlive = false;
+        }
+    }
 
-	//每帧执行一次，检测玩家是否存活
-	void Update () {	
-		if (health <= 0)
-			isAlive = false;
-	}
-
-	//玩家扣血函数，用于GameManager脚本中调用
-	public void TakeDamage(int damage){
-		health -= damage;
-		if (health < 0) 
-			health = 0;
-	}
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            health = 0;
+        }
+    }
 }
